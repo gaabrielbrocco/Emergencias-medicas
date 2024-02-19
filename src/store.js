c103a4e51efcf60b224500a28dd3b85c00925bd5
Gabriel <gabriel@webcgi.com.br>
2024-02-19 09:03:02 -0300

    Enviando projeto finalizado

---
+++ src/store.js
@@ -0,0 +1,100 @@
+import Vuex from 'vuex'
+
+export default new Vuex.Store({
+    state: {
+        titulo: 'Emergências Médicas',
+        equipe: {
+            enfermeiro: '',
+            socorrista: '',
+            medico: '',
+            carro: '',
+            telefone: '',
+            kitDeReanimacao: ''
+        },
+        equipes: [],
+        enfermeiros: [],
+        socorristas: [],
+        medicos: [],
+        equipamentos: {
+            carros: [],
+            telefones: [],
+            kitsDeReanimacao: []
+        }
+    },
+    getters: {
+        totalEnfermeiros(state) {
+            return state.enfermeiros.length
+        },
+        socorristasPorTurno(state) { //closure
+            return turno => !turno ? state.socorristas : state.socorristas.filter(s => s.turno === turno)
+        },
+        totalSocorristas: state => state.socorristas.length,
+        totalSocorristasPorTurno: (state, getters) => turno => getters.socorristasPorTurno(turno).length
+    },
+    mutations: {
+        //setItemEquipe: (state, item) => {
+        setItemEquipe: (state, item) => {
+
+            let t = item.tipo
+            let d = item.dados
+
+            if(t == 'enfermeiros') state.equipe.enfermeiro = d.nome
+            if(t == 'socorristas') state.equipe.socorrista = d.nome
+            if(t == 'medicos') state.equipe.medico = d.nome
+            if(t == 'carros') state.equipe.carro = d.placa
+            if(t == 'telefones') state.equipe.telefone = d.telefone
+            if(t == 'kits-de-reanimacao') state.equipe.kitDeReanimacao = d.kit
+            
+        },
+        setEnfermeiros: (state, payload) => {
+            state.enfermeiros = payload
+        },
+        setSocorristas: (state, payload) => {
+            state.socorristas = payload
+        },
+        setMedicos: (state, payload) => {
+            state.medicos = payload
+        },
+        setCarros: (state, payload) => {
+            state.equipamentos.carros = payload
+        },
+        setTelefones: (state, telefones) => {
+            state.equipamentos.telefones = telefones
+        },
+        setKitsDeReanimacao: (state, payload) => {
+            state.equipamentos.kitsDeReanimacao = payload
+        },
+        adicionarEquipeEmEquipes: (state, payload) => {
+            state.equipes.push(payload)
+            state.equipe = {}
+            console.log(state.equipes)
+        }
+    },
+    actions: {
+        fetchEquipamentos(context, { carros, telefones, kitsDeReanimacao }) {
+            fetch('http://localhost:3000/equipamentos')
+                .then(response => response.json())
+                .then(dados => {
+                    if(carros) context.commit('setCarros', dados.carros)
+                    //processamento assíncrono
+                    if(telefones) context.commit('setTelefones', dados.telefones)
+                    //processamento assíncrono
+                    //diversas regras de negócio
+                    if(kitsDeReanimacao) context.commit('setKitsDeReanimacao', dados.kitsDeReanimacao)
+                })
+        },
+        fetchProfissionais(context) {
+            fetch('http://localhost:3000/enfermeiros')
+                .then(response => response.json())
+                .then(dados => context.commit('setEnfermeiros', dados))
+
+            fetch('http://localhost:3000/socorristas')
+                .then(response => response.json())
+                .then(dados => context.commit('setSocorristas', dados))
+
+            fetch('http://localhost:3000/medicos')
+                .then(response => response.json())
+                .then(dados => context.commit('setMedicos', dados))
+        }
+    }
+})
